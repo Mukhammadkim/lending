32ca2422cc92351055923b542cf0ebe7f461667b
Mukhammadkim <111438361+Mukhammadkim@users.noreply.github.com>
2024-01-21 21:45:47 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,326 @@
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->  
+<!--[if IE 9]> <html lang="en" class="ie9"> <![endif]-->  
+<!--[if !IE]><!--> <html lang="en"> <!--<![endif]-->  
+<head>
+    <title>Responsive website template for developers</title>
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Responsive HTML5 website landing page for developers and startups">
+    <meta name="author" content="Xiaoying Riley at 3rd Wave Media">    
+    <link rel="shortcut icon" href="favicon.ico">  
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <!-- Global CSS -->
+    <link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">   
+    <!-- Plugins CSS -->
+    <link rel="stylesheet" href="assets/plugins/font-awesome/css/font-awesome.css">
+    <!-- Theme CSS -->  
+    <link id="theme-style" rel="stylesheet" href="assets/css/styles.css">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    
+</head> 
+
+<body>
+    <!-- ******HEADER****** --> 
+    <header id="header" class="header">  
+        <div class="container">       
+            <h1 class="logo">
+                <a class="scrollto" href="#hero">
+                    <span class="logo-icon-wrapper"><img class="logo-icon" src="assets/images/logo-icon.svg" alt="icon"></span>
+                    <span class="text"><span class="highlight">APP</span>KIT</span></a>
+            </h1><!--//logo-->
+            <nav class="main-nav navbar-right" role="navigation">
+                <div class="navbar-header">
+                    <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button><!--//nav-toggle-->
+                </div><!--//navbar-header-->
+                <div id="navbar-collapse" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="active nav-item"><a class="scrollto" href="#about">About</a></li>
+                        <li class="nav-item"><a class="scrollto" href="#testimonials">Testimonials</a></li>
+                        <li class="nav-item"><a class="scrollto" href="#features">Features</a></li>                        
+                        <li class="nav-item"><a class="scrollto" href="#team">Team</a></li>
+                        <li class="nav-item"><a class="scrollto" href="#pricing">Pricing</a></li>
+                        <li class="nav-item"><a class="scrollto" href="#contact">Contact</a></li>
+                    </ul><!--//nav-->
+                </div><!--//navabr-collapse-->
+            </nav><!--//main-nav-->                     
+        </div><!--//container-->
+    </header><!--//header-->
+    
+    <div id="hero" class="hero-section">
+        
+        <div id="hero-carousel" class="hero-carousel carousel carousel-fade slide" data-ride="carousel" data-interval="10000">
+            
+            <div class="figure-holder-wrapper">
+        		<div class="container">
+            		<div class="row">
+                		<div class="figure-holder">
+                	        <img class="figure-image img-responsive" src="assets/images/imac.png" alt="image" />
+                        </div><!--//figure-holder-->
+            		</div><!--//row-->
+        		</div><!--//container-->
+    		</div><!--//figure-holder-wrapper-->
+            
+			<!-- Indicators -->
+			<ol class="carousel-indicators">
+				<li class="active" data-slide-to="0" data-target="#hero-carousel"></li>
+				<li data-slide-to="1" data-target="#hero-carousel"></li>
+			</ol>
+			
+			<!-- Wrapper for slides -->
+			<div class="carousel-inner">
+    			
+				<div class="item item-1 active">
+					<div class="item-content container">
+    					<div class="item-content-inner">
+    				        
+				            <h2 class="heading">ОБУЧАЕМ ТЕНДЕРАМ, <br class="hidden-xs">ГОСЗАКУПКАМ ОНЛАЙН <br class="hidden-xs">ТАКЖЕ ОФФЛАЙН</h2>
+				            <p class="intro">Вы можете обучатся у профессионалов всего за 3 недели либо потерять 3 года, пробуя работать самостоятельно</p>
+				            <a class="btn btn-primary btn-cta" href="https://offmedia.tilda.ws/tenderastana2" target="_blank">ЗАРЕГЕСТРИРОВАТЬСЯ НА ОБУЧЕНИЕ</a>
+    				        
+    					</div><!--//item-content-inner-->
+					</div><!--//item-content-->
+				</div><!--//item-->
+				
+				<div class="item item-2">
+					<div class="item-content container">
+						<div class="item-content-inner">
+    				        
+				            <h2 class="heading">almat_kaldybekuly</h2>
+				            <p class="intro">ТЕНДЕР ЭКСПЕРТ • ОБУЧЕНИЕ • НАСТАВНИЧЕСТВО<br class="hidden-xs">💸 Финансирование до 50 млн<br class="hidden-xs">💯ОФФЛАЙН<br class="hidden-xs">📓 Сопровождение сделок БЕЗ РИСКА</p>
+				            <a class="btn btn-primary btn-cta" href="https://www.instagram.com/almat_kaldybekuly/" target="_blank">ИНСТАГРАММ</a>
+    				        
+    					</div><!--//item-content-inner-->
+					</div>
+				</div><!--//item-->
+				
+			</div><!--//carousel-inner-->
+
+		</div><!--//carousel-->
+    </div><!--//hero-->
+    
+    <div id="about" class="about-section">
+        <div class="container text-center">
+            <h2 class="section-title">Почему вы должны выбрать нас:</h2>
+            <p class="intro">Мы каждый день тяжело трудимся и пытаемся улучшаться</p>
+            <div class="items-wrapper row">
+                <div class="item col-sm-4 col-xs-12">
+                    <div class="item-inner">
+                        <div class="figure-holder">
+                            <img class="figure-image" src="assets/images/figure-1.png" alt="image">
+                        </div><!--//figure-holder-->
+                        <h3 class="item-title">Качественное обучение, нацеленное на достижение результата</h3>
+                        <div class="item-desc">
+                            Всегда актуальная информация, индивидуальные консультации для каждого ученика.
+                        </div><!--//item-desc-->
+                    </div><!--//item-inner-->
+                </div><!--//item-->
+                <div class="item col-sm-4 col-xs-12">
+                    <div class="item-inner">
+                        <div class="figure-holder">
+                            <img class="figure-image" src="assets/images/figure-2.png" alt="image">
+                        </div><!--//figure-holder-->
+                        <h3 class="item-title">Получение новой професии</h3>
+                        <div class="item-desc">
+                            Вы можете работать как фрилансер, предостовляя свои услуги или устроиться на работу заказчиком или поставщиком. </div><!--//item-desc-->
+                    </div><!--//item-inner-->
+                </div><!--//item-->
+                <div class="item col-sm-4 col-xs-12">
+                    <div class="item-inner">
+                        <div class="figure-holder">
+                            <img class="figure-image" src="assets/images/figure-3.png" alt="image">
+                        </div><!--//figure-holder-->
+                        <h3 class="item-title">Познание множество разных секретов нашей отрасли</h3>
+                        <div class="item-desc">
+                            В нашей компании проходят обучение руководители, основатели компании, специалисты из различных сфер. У нас вы получаете новое окружение, знакомства.</div><!--//item-desc-->
+                    </div><!--//item-inner-->
+                </div><!--//item-->
+            </div><!--//items-wrapper-->
+        </div><!--//container-->
+    </div><!--//about-section-->
+    
+    <div id="testimonials" class="testimonials-section">
+        <div class="container">
+            <h2 class="section-title text-center">Наши ученики любят нас:</h2>
+            <div class="item center-block">
+                <div class="profile-holder">
+                    <img class="profile-image" src="assets/images/profile-1.png" alt="profile">
+                </div>
+                <div class="quote-holder">
+                    <blockquote class="quote">
+                        <p>За 2 недели в этой отрасли, выиграл 4 тендера, общая сумма этих тендеров составило 550 000 тг чистой прибылью.</p>
+                        <div class="quote-source">
+                            <span class="name">Алишер,</span>
+                            <span class="meta">Возраст: 22 лет</span>
+                        </div><!--//quote-source-->
+                    </blockquote>
+                </div><!--//quote-holder-->
+            </div><!--//item-->
+            <div class="item item-reversed center-block">
+                <div class="profile-holder">
+                    <img class="profile-image" src="assets/images/profile-2.png" alt="profile">
+                </div>
+                <div class="quote-holder">
+                    <blockquote class="quote">
+                        <p>За месяц выиграл тендер на 20 000 000. Очень хорошие наставники, и в то же время профессионалы своего дела. </p>
+                        <div class="quote-source">
+                            <span class="name">Диас,</span>
+                            <span class="meta">Возраст: 26 лет</span>
+                        </div><!--//quote-source-->
+                    </blockquote>
+                </div><!--//quote-holder-->
+            </div><!--//item-->
+            <div class="item center-block">
+                <div class="profile-holder">
+                    <img class="profile-image" src="assets/images/profile-3.png" alt="profile">
+                </div>
+                <div class="quote-holder">
+                    <blockquote class="quote">
+                        <p>Учился у других специалистов, но все же были огромные проблемы, некоторые недостатки были, но когда начал учиться в этой компании, все начало идти своим ходом, и за месяц вышел в плюс на 1 100 000 тг</p>
+                        <div class="quote-source">
+                            <span class="name">Улыкбек,</span>
+                            <span class="meta">Возраст: 35 лет</span>
+                        </div><!--//quote-source-->
+                    </blockquote>
+                </div><!--//quote-holder-->
+            </div><!--//item-->
+            
+        </div><!--//container-->
+    </div><!--//testimonials-->
+    
+    
+    
+    <div class="team-section" id="team">
+        <div class="container text-center">
+            <h2 class="section-title">ТАРИФЫ:</h2>
+            <div class="members-wrapper row">
+                <div class="item col-md-4 col-sm-4 col-xs-6">
+                    <div class="item-inner">
+                        <div class="profile">
+                            <img class="profile-image" src="assets/images/team-1.png" alt="Xiaoying Riley" />
+                        </div>
+                        
+                        <div class="member-content">
+                            <h3 class="member-name">Тариф «Стандарт»</h3>
+                            <div class="member-title">Оффлайн обучение 100% теория (портал Госзакуп и Самрук-Казына)</div>
+                            <div class="member-desc">
+                               <p>Электронный сертификат об окончании курса <br>Группа 20-25 человек</p>
+                            </div><!--//member-desc-->
+                        </div><!--//member-content-->
+                    </div><!--//item-inner-->
+                </div><!--//item-->
+                <div class="item col-md-4 col-sm-4 col-xs-6">
+                    <div class="item-inner">
+                        <div class="profile">
+                            <img class="profile-image" src="assets/images/team-2.png" alt="Tom Najdek" />
+                        </div>
+                        
+                        <div class="member-content">
+                            <h3 class="member-name">Тариф «Бизнес»</h3>
+                            <div class="member-title">Оффлайн обучение 50% теория,50% практика на портале Госзакуп и Самрук-Казына.</div>
+                            <div class="member-desc">
+                                <p>Бонус курс маркетплейс ОМаркет, Ск Маркет <br>Е факторинг курс Торговля с заводами Китай и СНГ Курс+Бонус склад, надёжный карго<br>Электронный сертификат об окончании курса<br>Группа 15-20 человек</p>
+                            </div><!--//member-desc-->
+                        </div><!--//member-content-->
+                    </div><!--//item-inner-->
+                </div><!--//item-->
+                <div class="item col-md-4 col-sm-4 col-xs-6">
+                    <div class="item-inner">
+                        <div class="profile">
+                            <img class="profile-image" src="assets/images/team-1.png" alt="Xiaoying Riley" />
+                        </div>
+                        
+                        <div class="member-content">
+                            <h3 class="member-name">Тариф «VIP»</h3>
+                            <div class="member-title">Оффлайн обучение 100 % практика, 100% теория на портале Госзакуп и Самрук-Казына. Теория по субботам 3 часа и консультация.</div>
+                            <div class="member-desc">
+                               <p>Бонус курс маркетплейс ОМаркет, Ск Маркет, готовые проекты <br>Финансирование до 100 млн тг.<br>Электронный сертификат об окончании курса<br>Группа 5-6 человек</p>
+                            </div><!--//member-desc-->
+                        </div><!--//member-content-->
+                    </div><!--//item-inner-->
+                </div><!--//item-->
+            </div><!--//members-wrapper-->
+        </div>
+    </div><!--//team-section-->
+    
+    <div id="pricing" class="pricing-section">
+        <div class="container text-center">
+            <h2 class="section-title">Наши услуги:</h2>
+            <div class="intro">"Один из обязанностей наставника - это помощь своим ученикам"</div>
+            <div class="pricing-wrapper">
+                <div class="item item-1 col-md-4 col-sm-2 col-xs-12">
+                    <div class="item-inner">
+                        <h3 class="item-heading">Тендерное сопровождение</h3>
+                        <div class="price-desc">
+                            <p>Мы даем комплексное тендерное сопровождение от наших ЭКСПЕРТОВ</p>
+                            
+                            
+                            
+                        </div><!--//price-desc-->
+                        
+                    </div><!--//item-inner-->
+                </div><!--//item-->
+                <div class="item item-1 col-md-4 col-sm-4 col-xs-12">
+                    <div class="item-inner">
+                        <h3 class="item-heading">Подбор тендеров</h3>
+                        <div class="price-desc">
+                            <p>Если вы не можете подобрать подходящий себе тендер, то мы помогаем вам с этим</p>
+                            
+                            
+                            
+                        </div><!--//price-desc-->
+                        
+                    </div><!--//item-inner-->
+                </div><!--//item-->
+                <div class="item item-1 col-md-4 col-sm-4 col-xs-12">
+                    <div class="item-inner">
+                        <h3 class="item-heading">Бесплатная поддержка и консультация</h3>
+                        <div class="price-desc">
+                            <p>Для новых клиентов которые не зарегестрированы на GOSZAKUP.GOV.KZ, мы предостовляем бесплатную поддержку и консультацию по тендерам</p>
+                            
+                            
+                            
+                        </div><!--//price-desc-->
+                        
+                    </div><!--//item-inner-->
+                </div><!--//item-->
+                
+            </div><!--//pricing-wrapper-->
+            
+        </div><!--//container-->
+    </div><!--//pricing-section-->
+    <div id="contact" class="contact-section">
+        <div class="container text-center">
+            <h2 class="section-title">Открой новый мир вместе с нами!</h2>
+            <div class="contact-content">
+                <p>Запишись на наше наставничество и выйди на новый уровень финансов!</p>
+            <h2 class="section-title">Адрес: БЦ АЛМА-АТА, ИМАНОВА 19, 8 ЭТАЖ, КАБ 808А</h2>
+            </div>
+            <a class="btn btn-cta btn-primary" href="https://wrapbootstrap.com/theme/admin-appkit-admin-theme-angularjs-WB051SCJ1?ref=3wm">Звонок</a>
+            <a class="btn btn-cta btn-primary" href="https://wrapbootstrap.com/theme/admin-appkit-admin-theme-angularjs-WB051SCJ1?ref=3wm">instagram</a>
+            <a class="btn btn-cta btn-primary" href="https://wrapbootstrap.com/theme/admin-appkit-admin-theme-angularjs-WB051SCJ1?ref=3wm">whatsapp</a>
+        </div><!--//container-->
+    </div><!--//contact-section-->
+    
+
+     
+    <!-- Javascript -->          
+    <script type="text/javascript" src="assets/plugins/jquery-1.12.3.min.js"></script>
+    <script type="text/javascript" src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="assets/plugins/jquery-scrollTo/jquery.scrollTo.min.js"></script>     
+    <script type="text/javascript" src="assets/js/main.js"></script> 
+       
+</body>
+</html> 
+
